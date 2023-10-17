356e123af49f7de870923047d3da4a59b9727bc0
federicanicoloits <127236809+federicanicoloits@users.noreply.github.com>
2023-10-17 22:16:58 +0200

    aggiunte focacce

--- src/app/focacce/focacce.component.ts
+++ src/app/focacce/focacce.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Meal } from '../meal.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-focacce',
@@ -6,5 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./focacce.component.css'],
 })
 export class FocacceComponent implements OnInit {
-  ngOnInit(): void {}
+  cards: Meal[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Meal[]>('./assets/focacce.json').subscribe((data) => {
+      this.cards = data;
+    });
+  }
 }
