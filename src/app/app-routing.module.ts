f4debff29a71cb8b972c42291b3ff269560c43d2
neagu robert alexandru <127115019+NeaguRobertAlexandru31@users.noreply.github.com>
2023-10-17 17:24:51 +0200

    tabs-menù-OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { ReservationsComponent } from './reservations/reservations.component';
 import { HomeComponent } from './home/home.component';
 import { PizzeRosseComponent } from './pizze-rosse/pizze-rosse.component';
 import { PizzeBiancheComponent } from './pizze-bianche/pizze-bianche.component';
+import { FocacceComponent } from './focacce/focacce.component';
+import { BevandeComponent } from './bevande/bevande.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'reservations', component: PizzeRosseComponent },
   { path: 'reservations/pizze-rosse', component: PizzeRosseComponent },
   { path: 'reservations/pizze-bianche', component: PizzeBiancheComponent },
+  { path: 'reservations/focacce', component: FocacceComponent },
+  { path: 'reservations/bevande', component: BevandeComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
 ];
 
