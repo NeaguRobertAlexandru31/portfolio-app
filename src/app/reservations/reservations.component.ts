0e843145879d74ea5d3c7de0a811dfd545e8a74b
federicanicoloits <127236809+federicanicoloits@users.noreply.github.com>
2023-10-18 07:44:39 +0200

    css+pills

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
 })
 export class ReservationsComponent {
   searchTerm: string = '';
-  selectedPill: string = 'rosse';
+
+  selectPill = {
+    rosse: 'rosse',
+    bianche: 'bianche',
+    focacce: 'focacce',
+    bevande: 'bevande',
+  };
+
+  selectedPill: string = this.selectPill.rosse;
+
+  selectPillFunction(scelta: string) {
+    switch (scelta) {
+      case this.selectPill.rosse:
+        this.selectedPill = this.selectPill.rosse;
+        break;
+      case this.selectPill.bianche:
+        this.selectedPill = this.selectPill.bianche;
+        break;
+      case this.selectPill.focacce:
+        this.selectedPill = this.selectPill.focacce;
+        break;
+      case this.selectPill.bevande:
+        this.selectedPill = this.selectPill.bevande;
+        break;
+    }
+  }
 }
