90b8a8b88cb7f81a5312699893d6936c19c31604
StefanoGoffi <127967639+StefanoGoffi@users.noreply.github.com>
2023-10-18 12:58:35 +0200

    modale semi completa
    risolto bug di apertura, da ridefinire la struttura e il blocco schermo

--- src/app/modale/modale.component.ts
+++ src/app/modale/modale.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modale.component.css'],
 })
 export class ModaleComponent implements OnInit {
+  // Quando si apre la modale
+$("#overlay").addClass("modal-open");
+$("body").addClass("modal-open");
+
+// Quando si chiude la modale
+$("#overlay").removeClass("modal-open");
+$("body").removeClass("modal-open");
+
   constructor() {}
 
   ngOnInit() {}
